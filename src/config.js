f648f3b83bf860c031689e8cbef5d91eb95afbc0
Hans-Georg Beyer <aero@schoash.com>
2019-12-07 23:19:06 +0100

    Setting up React app.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "hgbnotes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://cso213nnyd.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_SBM6ANJiC",
+      APP_CLIENT_ID: "r28647m6hfnh3cme4308qoja3",
+      IDENTITY_POOL_ID: "us-east-1:bf05aa6a-1bba-4acd-8edc-8293076d22bb"
+    }
+  };
